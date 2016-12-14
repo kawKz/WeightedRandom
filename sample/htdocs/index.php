095e7a2ad7eef538c2b46197d79b8b211c4601b8
kawKz <kawdongjing@gmail.com>
2016-12-14 18:42:08 +0900

    READMEの記述

--- sample/htdocs/index.php
+++ sample/htdocs/index.php
@@ -3,11 +3,12 @@ require_once (dirname(__FILE__) . '/../../src/Base.php');
 require_once (dirname(__FILE__) . '/../../src/Drawer.php');
 require_once (dirname(__FILE__) . '/../../src/Util.php');
 
-$instance = \WeightedRandom\Util::newInstance();
+//$instance = \WeightedRandom\Util::newInstance();
+$instance = new WeightedRandom\Drawer();
 $lots = array(
-    'A'=>'<font size=3 color="#1B0CF5">80%の確率です</font>',
-    'B'=>'<font size=5 color="#0CF569">15%の確率です</font>',
-    'C'=>'<b><font size=10 color="#F5650C">4%の確率です</font></b>',
+    'A'=>'<font size=3 color="#1B0CF5">60%の確率です</font>',
+    'B'=>'<font size=5 color="#0CF569">30%の確率です</font>',
+    'C'=>'<b><font size=10 color="#F5650C">9%の確率です</font></b>',
     'D'=>'<b><font size=20 color="#F50C0C">1%の確率です</font></b>'
 );
 $instance->setWeights(array(
